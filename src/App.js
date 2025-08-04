3c910d6f47446a3a980053dde7a4ca01e822e8b8
hanspeterhess <84492900+hanspeterhess@users.noreply.github.com>
2025-08-04 23:48:37 +0200

    authorizationParams updated

--- src/App.js
+++ src/App.js
@@ -376,9 +376,9 @@ export default function App() {
     <Auth0Provider
       domain={process.env.REACT_APP_AUTH0_DOMAIN}
       clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
-      redirectUri={window.location.origin}
       authorizationParams={{
-        audience: {process.env.REACT_APP_AUTH0_AUDIENCE},
+        redirect_uri: window.location.origin,
+        audience: process.env.REACT_APP_AUTH0_AUDIENCE,
         scope: "openid profile email"
       }}
     >
